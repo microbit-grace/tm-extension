350f9dd68fc7c6e7de6d1df54c93dd2357aebd4f
Grace <145345672+microbit-grace@users.noreply.github.com>
2025-10-02 12:21:40 +0100

    Reorder blocks to green, purple, orange

--- custom.ts
+++ custom.ts
@@ -5,22 +5,25 @@
 */
 
 /**
- * Custom blocks
+ * TMMachineLearning blocks
  */
 //% weight=120 color=#3E80F6 icon="" block="Machine Learning"
 namespace TMMachineLearning {
     
     //% block="on green start"
+    //% weight=3
     export function onMLGreenStart(handler: () => void): void {
         SerialCommands.onCommand("green", handler)
     }
 
     //% block="on purple start"
+    //% weight=2
     export function onMLPurpleStart(handler: () => void): void {
         SerialCommands.onCommand("purple", handler)
     }
     
     //% block="on orange start"
+    //% weight=1
     export function onMLOrangeStart(handler: () => void): void {
         SerialCommands.onCommand("orange", handler)
     }
